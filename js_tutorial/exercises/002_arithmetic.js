1ea41e7471779db1ea49f8aed4ed88e3952308d0
Armaan-1993 <armaanmathew88@gmail.com>
2020-04-11 16:47:18 +0530

    Add the program for arithmetic operations

---
+++ js_tutorial/exercises/002_arithmetic.js
@@ -0,0 +1,79 @@
+// Addition of two numbers
+
+let a , b;
+function addition(a , b)
+{
+return (a + b);
+}
+console.log("The sum is " + addition(4 , 6));
+
+// Subtraction of two numbers
+
+function subtraction(a , b)
+{
+return (a - b);
+}
+console.log("The difference is " + subtraction(19 , 9));
+
+// Multiplication of two numbers
+
+function product(a , b)
+{
+return (a * b);
+}
+console.log("The product is " + product(45 ,2));
+
+// Division of two numbers (Quotient)
+
+function quotient(a , b)
+{
+return (a / b);
+}
+console.log("The quotient of the division is " + quotient(50 , 10));
+
+// Division of two numbers
+
+function remainder(a , b)
+{
+return (a % b);
+}
+console.log("The remainder of the division is " + remainder(34 ,8));
+
+// Comparison with greater than operator
+
+function greater(a ,b)
+{
+if (a > b)
+{
+return "The first number is greater";
+}
+return "The second number is greater";
+}
+console.log(greater( 3 ,6));
+
+// Comparison with less than operator
+
+function lesser(a ,b)
+{
+if (a < b)
+{
+return "The first number is lesser";
+}
+return "The second number is lesser";
+}
+console.log(lesser( 3 ,6));
+
+// Comparision with equality operator
+
+function equality(a , b)
+{
+if (a == b)
+{
+return "The numbers are equal";
+}
+return "The numbers are not equal";
+}
+console.log(equality(34 , 7));
+
+
+
