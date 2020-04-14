ee763564976d5cca8ee6b9e2ff153c56b78c4cde
Armaan-1993 <armaanmathew88@gmail.com>
2020-04-15 02:51:21 +0530

    Implemented the program with a function

--- js_tutorial/exercises/013_odd.js
+++ js_tutorial/exercises/013_odd.js
@@ -1,11 +1,12 @@
 const readlineSync = require("readline-sync");
 const num = readlineSync.question("Enter the number ");
 const oddOrEven = (num) => {
-if (num%2 == 0) {
-    return("The number is even");
-    }
-else
-    return("The number is odd");
+  if (num % 2 == 0) {
+    console.log("The number is even");
+  } else {
+    console.log("The number is odd");
+  }
 }
-console.log(oddOrEven(num));
+oddOrEven(num);
+
 
