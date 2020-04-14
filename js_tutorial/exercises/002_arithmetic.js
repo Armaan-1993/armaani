6d8cb83329a12f8231031ed4908e926f03efb82e
Armaan-1993 <armaanmathew88@gmail.com>
2020-04-15 02:33:54 +0530

    Alignment corrected for the program

--- js_tutorial/exercises/002_arithmetic.js
+++ js_tutorial/exercises/002_arithmetic.js
@@ -1,58 +1,58 @@
 // Addition of two numbers
 
-let a , b;
+let a, b;
 const addition = (a , b) => {
     return (a + b);
 }
-console.log("The sum is " + addition(4 , 6));
+console.log("The sum is " + addition(4, 6));
 
 // Subtraction of two numbers
 
 const subtraction = (a , b) => {
     return (a - b);
 }
-console.log("The difference is " + subtraction(19 , 9));
+console.log("The difference is " + subtraction(19, 9));
 
 // Multiplication of two numbers
 
 const product = (a , b) => {
     return (a * b);
 }
-console.log("The product is " + product(45 ,2));
+console.log("The product is " + product(45, 2));
 
 // Division of two numbers (Quotient)
 
 const quotient = (a , b) => {
     return (a / b);
 }
-console.log("The quotient of the division is " + quotient(50 , 10));
+console.log("The quotient of the division is " + quotient(50, 10));
 
 // Division of two numbers
 
 const remainder = (a , b) => {
     return (a % b);
 }
-console.log("The remainder of the division is " + remainder(34 ,8));
+console.log("The remainder of the division is " + remainder(34, 8));
 
 // Comparison with greater than operator
 
-const greater = (a ,b) => {
+const greater = (a , b) => {
     if (a > b) {
         return "The first number is greater";
     }
     return "The second number is greater";
 }
-console.log(greater( 3 ,6));
+console.log(greater(3 , 6));
 
 // Comparison with less than operator
 
-const lesser = (a ,b) => {
+const lesser = (a , b) => {
     if (a < b) {
         return "The first number is lesser";
     }
     return "The second number is lesser";
 }
-console.log(lesser( 3 ,6));
+console.log(lesser(3 , 6));
 
 // Comparision with equality operator
 
