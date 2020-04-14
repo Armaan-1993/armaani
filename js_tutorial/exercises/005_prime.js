4e38e16f1bf4d1323966bde14117d198d90a4a94
Armaan-1993 <armaanmathew88@gmail.com>
2020-04-15 02:39:48 +0530

    Program corrected with different logic

--- js_tutorial/exercises/005_prime.js
+++ js_tutorial/exercises/005_prime.js
@@ -1,18 +1,30 @@
 const readlineSync = require("readline-sync");
 const num = readlineSync.question("Enter the number ");
 const checkIfPrimeNumber = (num) => {
-if (num%num == 0 && num%1 == 0 && num%2 == 1 && num%3 != 0 && num%7 != 0 && num%5 != 0) {
-    return("The entered number is a prime number");
+  if (num == 1) {
+    console.log("1 is neither prime nor composite");
+  } else {
+    let isprime = true;
+    for (let i = 2; i <= num - 1 && num != i; i++) {
+      if (num % i === 0) {
+        isprime = false;
+        break;
+      } else {
+        isprime = true;
+      }
     }
-else if (num == 2 || num == 3 || num == 5 || num == 7) {
-    return("The entered number is a prime number");
+    if (isprime === true) {
+      console.log("The number is prime");
+    } else {
+      console.log("The number is not prime");
     }
-else if (num == 1 || num == 0) {
-    return("The entered number is neither prime nor composite");
-    }
-else
-    return("The entered number is a composite number");
+  }
 }
-console.log(checkIfPrimeNumber(num));
+checkIfPrimeNumber(num);
+
+
+
+
+
 
 
