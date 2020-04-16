d7c314ed90614b75248efac58fd938f89ada4706
Armaan-1993 <armaanmathew88@gmail.com>
2020-04-16 06:07:10 +0530

    Program to find the sum of array elements

---
+++ js_tutorial/exercises/006_sumofarrayelements.js
@@ -0,0 +1,13 @@
+//6. Find the sum of elements in a given array. Array should be an input to the program
+
+let array = [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 ,10];
+const sumOfElements = (num) => {
+let sum = 0;
+    for( let i=0 ; i<array.length ; i++) {
+        sum = array[i] + sum;
+    }
+    console.log("The sum of all the elements of the array is " + sum);
+}
+sumOfElements(array);
+
+
