bf95463c7578c019c18391a833f006c1d3672f01
Armaan-1993 <armaanmathew88@gmail.com>
2020-04-18 06:22:41 +0530

    Alignment and spacing corrections made

--- js_tutorial/exercises/006_sumofarrayelements.js
+++ js_tutorial/exercises/006_sumofarrayelements.js
@@ -3,11 +3,16 @@
 let array = [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 ,10];
 const sumOfElements = (num) => {
 let sum = 0;
-    for( let i=0 ; i<array.length ; i++) {
+    for(let i = 0; i < array.length; i++) {
         sum = array[i] + sum;
     }
     console.log("The sum of all the elements of the array is " + sum);
 }
 sumOfElements(array);
+
+/*Output:
+
+PS C:\Users\armaa\PycharmProjects\Learningjavascript\exercises> node .\006_sumofarrayelements.js
+The sum of all the elements of the array is 55*/
 
 
