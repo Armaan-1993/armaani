f4a27f7df7abfd77ad58db1b523a9bc36087a9c6
Armaan-1993 <armaanmathew88@gmail.com>
2020-04-21 18:32:37 +0530

    Corrections made

--- js_tutorial/exercises/020_alphabetcheck.js
+++ js_tutorial/exercises/020_alphabetcheck.js
@@ -1,21 +1,20 @@
 //20. Write a program to check if a character is an alphabet or not
 
-const chara = "a";
+const character = "1";
 const checkIfAlphabetUsingASCII = (chara) => {
     let num = chara.charCodeAt() 
     if ((num >= 65 && num <= 90) || (num >= 97 && num <= 122)) {
-        console.log("This character is an alphabet")
-    } 
-    else {
-        console.log("The character is not an alphabet");
+        console.log(`The character "${character}" is an alphabet`)
+    } else {
+        console.log(`The character "${character}" is not an alphabet`);
     }
 }
-checkIfAlphabetUsingASCII(chara)
+
+checkIfAlphabetUsingASCII(character)
 
 /*Output:
 
+The character "a" is an alphabet
 PS C:\Users\armaa\PycharmProjects\Learningjavascript\exercises> node .\020_alphabetcheck.js
-The character is not an alphabet
-PS C:\Users\armaa\PycharmProjects\Learningjavascript\exercises> node .\020_alphabetcheck.js
-This character is an alphabet*/
+The character "1" is not an alphabet*/
 
