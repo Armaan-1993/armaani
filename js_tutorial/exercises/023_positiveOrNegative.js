aebf407999d3364bbde4b99b1ad6dc1b26ab335e
Armaan-1993 <armaanmathew88@gmail.com>
2020-04-21 17:24:09 +0530

    Corrections made

--- js_tutorial/exercises/023_positiveOrNegative.js
+++ js_tutorial/exercises/023_positiveOrNegative.js
@@ -1,25 +1,21 @@
 //23. Write a program to check a number is positive or negative
 
-let num = 67;
-const positiveOrNegative = (num) => {
-    if (num < 0) {
-        console.log(`The number ${num} is negative`);
-    }
-    else if (num > 0) {
-        console.log(`The number ${num} is positive`);
-    }
-    else if (num === 0) {
-        console.log(`The number ${num} is neither positive nor negative`);
+let num = -67;
+const positiveOrNegative = (number) => {
+    if (number < 0) {
+        console.log(`The number ${number} is negative`);
+    } else if (number > 0) {
+        console.log(`The number ${number} is positive`);
+    } else if (number === 0) {
+        console.log(`The number ${number} is neither positive nor negative`);
     }
 }
+
 positiveOrNegative(num);
 
 /*Output:
 
 PS C:\Users\armaa\PycharmProjects\Learningjavascript\exercises> node .\023_positiveOrNegative.js
-The number 0 is neither positive nor negative
+The number 67 is positive
 PS C:\Users\armaa\PycharmProjects\Learningjavascript\exercises> node .\023_positiveOrNegative.js
-The number -3 is negative
-PS C:\Users\armaa\PycharmProjects\Learningjavascript\exercises> node .\023_positiveOrNegative.js
-The number 67 is positive*/
-
+The number -67 is negative*/
