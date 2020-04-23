e2ea5f026c3cd4db0c271ae75e655bc19f8734f6
Armaan-1993 <armaanmathew88@gmail.com>
2020-04-23 07:08:38 +0530

    Corrections made

--- js_tutorial/exercises/008_sumofn.js
+++ js_tutorial/exercises/008_sumofn.js
@@ -1,17 +1,17 @@
 const readlineSync = require("readline-sync");
 const num = readlineSync.question("Enter the number ");
 const sumOfNNumbers = (num) => {
-sum = 0;
-if (num >= 1) {
-    for (let i = 1; (i <= num); i++) {
-        sum += i;
-    }
-    console.log("The sum of all the numbers from '1' to " + num + " is " + sum);
-    }
-else {
-    console.log("Enter a number greater than 0")
+    sum = 0;
+    if (num >= 1) {
+        for (let i = 1; (i <= num); i++) {
+            sum += i;
+        }
+        console.log("The sum of all the numbers from '1' to " + num + " is " + sum);
+    } else {
+        console.log("Enter a number greater than 0");
     }
 }
+
 sumOfNNumbers(num);
 
 /*Output:
