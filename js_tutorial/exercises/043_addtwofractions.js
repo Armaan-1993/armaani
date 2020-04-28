0e5eb6caf17aa5149b46869350a702333ec5baba
Armaan-1993 <armaanmathew88@gmail.com>
2020-04-28 07:20:21 +0530

    Program to add two fractions

---
+++ js_tutorial/exercises/043_addtwofractions.js
@@ -0,0 +1,12 @@
+//43. Add two fractions
+
+const addFractions = (num1 ,num2) => {
+    console.log(`The sum is ${num1 + num2}`);
+}
+
+addFractions(3/7 ,3/4);
+
+/*Output:
+
+PS C:\Users\armaa\PycharmProjects\Learningjavascript\exercises> node .\043_addtwofractions.js
+The sum is 1.1785714285714286*/
