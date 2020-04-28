80b02426d6ba6b21cae755ed4b3a0fb73685a3c0
Armaan-1993 <armaanmathew88@gmail.com>
2020-04-28 06:54:11 +0530

    Program to check if its a leap year or not

---
+++ js_tutorial/exercises/037_ifleapyearornot.js
@@ -0,0 +1,25 @@
+//37. Leap year or not
+
+const leapYear = (year) => {
+    isleapyear = false;
+    if (year % 4 === 0) {
+        isleapyear = true;
+    } else if (year % 100 === 0) {
+        isleapyear = false;
+    }    
+    if (isleapyear === true) {
+        console.log(`The year ${year} is a leap year`);
+    } else {
+        console.log(`The year ${year} is not a leap year`);
+    }
+}
+
+leapYear(2021);
+
+
+/*Ouput:
+
+PS C:\Users\armaa\PycharmProjects\Learningjavascript\exercises> node .\037_ifleapyearornot.js
+The year 2021 is not a leap year
+PS C:\Users\armaa\PycharmProjects\Learningjavascript\exercises> node .\037_ifleapyearornot.js
+The year 2020 is a leap year*/
