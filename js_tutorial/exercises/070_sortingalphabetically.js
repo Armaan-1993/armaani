bfa2de7276c9c0e79c81893d73598da0296e380b
Armaan-1993 <armaanmathew88@gmail.com>
2020-05-05 03:57:11 +0530

    Program to sort a string alphabetically

---
+++ js_tutorial/exercises/070_sortingalphabetically.js
@@ -0,0 +1,7 @@
+//70. Sorting a string in alphabetical order
+
+const sortingAlphabetically = (string) => {
+    return stringArray = string.toLowerCase().split("").sort();
+}
+
+console.log(sortingAlphabetically("rhetorical"));
