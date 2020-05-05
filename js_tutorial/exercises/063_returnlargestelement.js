e88387f6a5b227bd289381d37d9867b4d8075325
Armaan-1993 <armaanmathew88@gmail.com>
2020-05-05 14:59:42 +0530

    Program to print the largest number in a list

---
+++ js_tutorial/exercises/063_returnlargestelement.js
@@ -0,0 +1,17 @@
+// 63. Write a function that returns the largest element in a list.
+const largestElement = (elements) => {
+  let output = 0;
+  for (let i = 0; i <= output; i++) {
+    if (elements[i] > output) {
+      output = elements[i];
+    }
+  }
+  return output;
+}
+  
+console.log(largestElement('15485632458'));
+
+/*Output:
+
+8*/
+  
